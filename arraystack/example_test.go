011263398810274fac4b3ecdc41a3440a2637215
曾江彬 <zengjiangbin@lingxing.com>
2022-07-29 09:54:11 +0800

    ds[顺序栈]

--- arraystack/example_test.go
+++ arraystack/example_test.go
@@ -5,11 +5,7 @@ import "fmt"
 func Example() {
 	s := New()
 	s.Push(1).Push(2).Push(3).Push(4)
-	for {
-		if v := s.Pop(); v != nil {
-			fmt.Println(v)
-		} else {
-			break
-		}
+	for v := s.Pop(); v != nil; v = s.Pop() {
+		fmt.Println(v)
 	}
 }
--- main.go
+++ main.go
@@ -9,11 +9,7 @@ func main() {
 	// demo1
 	s := arraystack.New()
 	s.Push(1).Push(2).Push(3).Push(4)
-	for {
-		if v := s.Pop(); v != nil {
-			fmt.Println(v)
-		} else {
-			break
-		}
+	for v := s.Pop(); v != nil; v = s.Pop() {
+		fmt.Println(v)
 	}
 }
